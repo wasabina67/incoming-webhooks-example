4c81f169180442f162a94e5ed476e76f0ffaa311
wasabina67 <wasabina67@gmail.com>
2024-08-15 01:23:33 +0900

    Update send.py

--- send.py
+++ send.py
@@ -1,5 +1,17 @@
+import json
+import os
+
+import requests
+from dotenv import load_dotenv
+
+
 def main():
-    pass
+    load_dotenv(override=True)
+    url = os.getenv("WEB_HOOKS_URL")
+
+    data = json.dumps({"text": "text"})
+    resp = requests.post(url=url, data=data)
+    print(resp.json)
 
 
 if __name__ == "__main__":
